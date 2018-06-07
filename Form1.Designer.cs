596e8dfc70b3a34efc473594b7c1175f18869399
Asif67 <39729439+Asif67@users.noreply.github.com>
2018-06-07 22:36:16 +0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace NWUClassRoutine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loginInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nWUClassRoutineDataSet = new NWUClassRoutine.NWUClassRoutineDataSet();
+            this.loginInfoTableAdapter = new NWUClassRoutine.NWUClassRoutineDataSetTableAdapters.LoginInfoTableAdapter();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.loginInfoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nWUClassRoutineDataSet)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.loginInfoBindingSource, "UserName", true));
+            this.textBox1.Location = new System.Drawing.Point(39, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // loginInfoBindingSource
+            // 
+            this.loginInfoBindingSource.DataMember = "LoginInfo";
+            this.loginInfoBindingSource.DataSource = this.nWUClassRoutineDataSet;
+            // 
+            // nWUClassRoutineDataSet
+            // 
+            this.nWUClassRoutineDataSet.DataSetName = "NWUClassRoutineDataSet";
+            this.nWUClassRoutineDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // loginInfoTableAdapter
+            // 
+            this.loginInfoTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.loginInfoBindingSource, "Passwords", true));
+            this.textBox2.Location = new System.Drawing.Point(39, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(76, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(114, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loginInfoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nWUClassRoutineDataSet)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private NWUClassRoutineDataSet nWUClassRoutineDataSet;
+        private System.Windows.Forms.BindingSource loginInfoBindingSource;
+        private NWUClassRoutineDataSetTableAdapters.LoginInfoTableAdapter loginInfoTableAdapter;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NWUClassRoutine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'nWUClassRoutineDataSet.LoginInfo' table. You can move, or remove it, as needed.
+            this.loginInfoTableAdapter.Fill(this.nWUClassRoutineDataSet.LoginInfo);
+            loginInfoBindingSource.DataSource = this.nWUClassRoutineDataSet.LoginInfo;
+            panel1.Enabled = false;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel1.Enabled = true;
+                this.nWUClassRoutineDataSet.LoginInfo.AddLoginInfoRow(this.nWUClassRoutineDataSet.LoginInfo.NewLoginInfoRow());
+                loginInfoBindingSource.MoveLast();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Messsage", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                loginInfoBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                loginInfoBindingSource.EndEdit();
+                loginInfoTableAdapter.Update(this.nWUClassRoutineDataSet.LoginInfo);
+                panel1.Enabled = false;
+                MessageBox.Show("Record Added to Database");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Messsage", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                loginInfoBindingSource.ResetBindings(false);
+            }
+        }
+    }
+}
